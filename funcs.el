112625e39331fb1d9d353e0bd466a5ed17ecab61
Dan Girshovich <redacted@example.com>
2020-04-14 02:39:24 -0700

    Add tangle files

---
+++ funcs.el
@@ -0,0 +1,191 @@
+;;; ~/.doom.d/funcs.el -*- lexical-binding: t; -*-
+
+(defun my/goto-literate-private-config-file ()
+  "Open the private config.org file."
+  (interactive)
+  (find-file (expand-file-name "config.org" doom-private-dir)))
+
+(defun my/rot13-and-kill-region ()
+  (interactive)
+  (kill-new (rot13 (buffer-substring (region-beginning) (region-end)))))
+
+(defun my/org-export-subtree-as-markdown-and-copy ()
+  (interactive)
+  (save-window-excursion
+    (let ((export-buffer (org-md-export-as-markdown nil t nil)))
+      (with-current-buffer export-buffer
+        (clipboard-kill-ring-save (point-min) (point-max)))
+      (kill-buffer export-buffer))))
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (call-interactively 'goto-line))
+    (linum-mode -1)))
+
+(defun my-increment-number-decimal
+    (&optional
+     arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by
+              (if arg
+                  arg
+                1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0)
+                               (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d") answer)))))))
+
+(defun rev-other-window ()
+  (interactive)
+  (other-window -1))
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+     current buffer's file. The eshell is renamed to match that
+     directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (name   (car (last (split-string parent "/" t)))))
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(defun treemax-save-shebanged-file-as-executable ()
+  (and (save-excursion
+         (save-restriction
+           (widen)
+           (goto-char (point-min))
+           (save-match-data
+             (looking-at "^#!"))))
+       (not (file-executable-p buffer-file-name))
+       (shell-command (concat "chmod +x " buffer-file-name))
+       (message
+        (concat "Saved as script: " buffer-file-name))))
+
+;; https://www.emacswiki.org/emacs/CopyingWholeLines
+(defun my/duplicate-line-or-region (&optional n)
+  "Duplicate current line, or region if active.
+With argument N, make N copies.
+With negative N, comment out original line and use the absolute value."
+  (interactive "*p")
+  (let ((use-region (use-region-p)))
+    (save-excursion
+      (let ((text (if use-region        ; Get region if active, otherwise line
+                      (buffer-substring (region-beginning) (region-end))
+                    (prog1 (thing-at-point 'line)
+                      (end-of-line)
+                      (if (< 0 (forward-line 1)) ; Go to beginning of next line, or make a new one
+                          (newline))))))
+        (dotimes (i (abs (or n 1)))     ; Insert N times, or once if not specified
+          (insert text))))
+    (if use-region nil                  ; Only if we're working with a line (not a region)
+      (let ((pos (- (point) (line-beginning-position)))) ; Save column
+        (if (> 0 n)                             ; Comment out original with negative arg
+            (comment-region (line-beginning-position) (line-end-position)))
+        (forward-line 1)
+        (forward-char pos)))))
+
+(defun my/org-ref-noter-link-from-arxiv (arxiv-number)
+  "Retrieve a pdf for ARXIV-NUMBER and save it to the default PDF dir.
+Then, add a bibtex entry for the new file in the default bib
+file. Then, create a new org-ref note heading for it (see
+org-ref-create-notes-hook in packages.el to see it also creates
+an property for org-noter). Finally, insert a descriptive link to
+the note heading at point, using the paper title as the link
+text.
+"
+  (interactive "sarxiv number: ")
+  (let ( (bibtex-dialect 'BibTeX))
+    (org-ref-save-all-bibtex-buffers)
+    (save-window-excursion
+      (arxiv-get-pdf-add-bibtex-entry arxiv-number
+                                      (car org-ref-default-bibliography)
+                                      org-ref-pdf-directory)
+      (org-ref-save-all-bibtex-buffers))
+    (let* ((parsed-entry (save-excursion
+                           (with-temp-buffer
+                             (insert-file-contents (car org-ref-default-bibliography))
+                             (bibtex-set-dialect (parsebib-find-bibtex-dialect) t)
+                             (search-forward (format "{%s}" arxiv-number))
+                             (bibtex-narrow-to-entry)
+                             (bibtex-beginning-of-entry)
+                             (bibtex-parse-entry)))))
+      (org-insert-heading)
+      (let* ((raw-ref-title (cdr (assoc "title" parsed-entry)))
+             (ref-title (s-replace-regexp (rx (sequence "\n" (+ space))) " "
+                                          (car (cdr (s-match (rx "{" (group (+ anything)) "}") raw-ref-title)))))
+             (ref-key (cdr (assoc "=key=" parsed-entry))))
+        (insert ref-title)
+        (insert "\n\n")
+        (insert (format "cite:%s" ref-key))))))
+
+(defun my/set-redshift (level)
+  (interactive "nRedshift level: ")
+  (shell-command (format "redshift -O %s" level)))
+
+(defun my/night-mode ()
+  (interactive)
+  (my/set-brightness 10)
+  (my/set-redshift 1500))
+
+(defun my/day-mode ()
+  (interactive)
+  (my/set-brightness 1000)
+  (my/set-redshift 6000))
+
+
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+Intelligently means: region, subtree, or defun, whichever applies
+first.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+        (t (narrow-to-defun))))
+
+;; https://stackoverflow.com/questions/28727190/org-babel-tangle-only-one-code-block
+(defun my/org-babel-tangle-block()
+  (interactive)
+  (let ((current-prefix-arg '(4)))
+    (call-interactively 'org-babel-tangle)))
+
+(defun my/open-org-files-list ()
+  (delq nil
+        (mapcar (lambda (buffer)
+                  (buffer-file-name buffer))
+                (org-buffer-list 'files t))))
+
+(defun my/save-shebanged-file-as-executable ()
+  (and (save-excursion
+         (save-restriction
+           (widen)
+           (goto-char (point-min))
+           (save-match-data
+             (looking-at "^#!"))))
+       (not (file-executable-p buffer-file-name))
+       (shell-command (concat "chmod +x " buffer-file-name))
+       (message
+        (concat "Saved as script: " buffer-file-name))))
+
+(add-hook 'after-save-hook #'my/save-shebanged-file-as-executable)
---
+++ packages.el
@@ -0,0 +1,24 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+(package! aggressive-indent)
+(package! ace-window)
+(package! key-chord)
+(package! helpful)
+(package! phi-search)
+(package! undo-tree)
+(package! dired-narrow)
+(package! deadgrep)
+(package! dmenu)
+(package! company-posframe)
+(package! org-noter)
+(package! org-roam
+          :recipe (:host github :repo "jethrokuan/org-roam"))
+(package! company-org-roam
+  :recipe (:host github :repo "jethrokuan/company-org-roam"))
+
+(package! smartscan)
+(package! org-ref)
+(package! google-this)
+(package! wrap-region)
+(package! edit-server)
+(package! slack)

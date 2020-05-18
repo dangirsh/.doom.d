e5d8fe8e76bd0ec066a9efa90cbfe5ca3125b36d
Dan <dan.girsh@gmail.com>
2020-05-18 02:44:58 -0700

    tangles

--- modules/personal/neurosys/config.el
+++ modules/personal/neurosys/config.el
@@ -1,3 +1,18 @@
+(setq neurosys/base-dir "/home/dan/repos/neurosys/")
+
+(defun neurosys/deploy-to-host (host)
+  (interactive "sHost:")
+  (let* ((host-root (format "/ssh:%s:/" host)))
+    (save-window-excursion
+      (org-babel-tangle)
+      (my/run-in-fresh-compilation
+       (format (concat neurosys/base-dir "rsync.sh")
+               " %s" host))
+      ;; TODO: Is there cleaner way to compile over TRAMP?
+      (find-file host-root)
+      (compile "nixos-rebuild switch")))
+  (switch-to-buffer-other-window "*compilation*"))
+
 (defun neurosys/open-config-file ()
   (interactive)
   (find-file "/home/dan/repos/neurosys/README.org"))

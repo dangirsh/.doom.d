84184c7764542d29d28ce3f2bd3e252d951cb8c4
Dan <dan.girsh@gmail.com>
2020-11-23 17:30:49 +0100

    adding custom.el and mathpix.el

---
+++ custom.el
@@ -0,0 +1,64 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-safe-themes
+   '("6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "0cb1b0ea66b145ad9b9e34c850ea8e842c4c4c83abe04e37455a1ef4cc5b8791" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" default))
+ '(fci-rule-color "#D6D6D6")
+ '(jdee-db-active-breakpoint-face-colors (cons "#FFFBF0" "#268bd2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#FFFBF0" "#859900"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#FFFBF0" "#E1DBCD"))
+ '(mathpix-app-id "dan_girsh_gmail_com_5d68dc")
+ '(mathpix-app-key "600336b7b2b932549ce4")
+ '(mathpix-screenshot-method "scrot -s %s")
+ '(objed-cursor-color "#dc322f")
+ '(org-agenda-files
+   '("~/Sync/org-roam/20200724172904-ford_battery_modeling.org" "/home/dan/Sync/org-roam/todo.org"))
+ '(org-download-screenshot-method "xclip -selection clipboard -t image/png -o > %s")
+ '(org-journal-carryover-items "TODO=\"TODO\"")
+ '(org-journal-date-format "%Y-%m-%d")
+ '(org-journal-date-prefix "#+TITLE: ")
+ '(org-journal-dir "/home/dan/Sync/org-roam/journal")
+ '(org-journal-file-format "private-%Y-%m-%d.org")
+ '(org-journal-time-format "")
+ '(org-journal-time-prefix "* ")
+ '(org-noter-always-create-frame t)
+ '(org-noter-notes-window-behavior '(start))
+ '(org-noter-notes-window-location 'horizontal-split)
+ '(org-roam-bibtex-mode t t)
+ '(pdf-view-midnight-colors (cons "#556b72" "#FDF6E3"))
+ '(rustic-ansi-faces
+   ["#FDF6E3" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#556b72"])
+ '(vc-annotate-background "#FDF6E3")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#859900")
+    (cons 40 "#959300")
+    (cons 60 "#a58e00")
+    (cons 80 "#b58900")
+    (cons 100 "#bc7407")
+    (cons 120 "#c35f0e")
+    (cons 140 "#cb4b16")
+    (cons 160 "#cd4439")
+    (cons 180 "#d03d5d")
+    (cons 200 "#d33682")
+    (cons 220 "#d63466")
+    (cons 240 "#d9334a")
+    (cons 260 "#dc322f")
+    (cons 280 "#dd5c56")
+    (cons 300 "#de867e")
+    (cons 320 "#dfb0a5")
+    (cons 340 "#D6D6D6")
+    (cons 360 "#D6D6D6")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-roam-link ((t (:inherit org-link)))))

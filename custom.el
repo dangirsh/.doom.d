9bb35e379544dedd85bd3beb77a3a153bd7c9952
Dan Girshovich <dan@worldcoin.org>
2022-06-18 19:18:20 +0200

    add my/org-titlify-link-or-noop

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" default))
+ '(safe-local-variable-values
+   '((eval setq-local org-roam-db-location "/home/dan/Work/Worldcoin/org-roam/org-roam.db")
+     (eval setq-local org-roam-directory "/home/dan/Work/Worldcoin/org-roam")
+     (org-roam-db-location . "/home/dan/Work/Worldcoin/org-roam/org-roam.db")
+     (org-roam-directory . "/home/dan/Work/Worldcoin/org-roam"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

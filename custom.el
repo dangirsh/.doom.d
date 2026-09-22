f480541e8bffb3602a9096677a9852a48b7db06c
Dan Girshovich <dangirsh@users.noreply.github.com>
2026-09-22 11:20:41 +0200

    Fix second WORK-PATH occurrence missed by non-global sed

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4" "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("/home/dan/Work/w/org-roam/daily/2024-10-14.org" "/home/dan/<WORK-PATH>/org-roam/orgzly/work-todo.org" "/home/dan/Sync/org-roam2/orgzly/todo.org" "/home/dan/Sync/org-roam2/orgzly/pixel-incoming.org"))
+   '("/home/dan/Work/w/org-roam/daily/2024-10-14.org" "/home/dan/Work/w/org-roam/orgzly/work-todo.org" "/home/dan/Sync/org-roam2/orgzly/todo.org" "/home/dan/Sync/org-roam2/orgzly/pixel-incoming.org"))
  '(package-selected-packages '(eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
